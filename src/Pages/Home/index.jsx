cefce15ce2cfd2144484fd2f9fae7d67a7a76e9b
Felipe Uenes <uenesdesigner@gmail.com>
2024-03-24 16:54:02 -0300

    add

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,15 @@
+
+import "./style.css"
+import ImageHome from "../../assets/avaliacao-home.png";
+
+export function Home(){
+    
+    
+    
+    return(
+        <div className="displayHome">
+            <section><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis reprehenderit adipisci animi tempore explicabo accusantium eos fugiat deserunt, ducimus necessitatibus voluptate est aspernatur! Dolores ipsa odio, deleniti fugit quaerat aliquid.</p></section>
+            <section><img src={ImageHome} alt="" /></section>
+        </div>
+    )
+}
