38043eeab4cb2db8401579c4fbdd326a6f4de560
Felipe Uenes <uenesdesigner@gmail.com>
2024-03-26 20:25:07 -0300

    ajuste de css descktop

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,9 +9,11 @@ export function Home(){
     return(
         <div className="displayHome">
             <section>
-                <h2 className="titulo1">Deixe aqui o seu</h2>
+                <h2 className="titulo1">DEIXE AQUI O SEU</h2>
                 <h1 className="titulo2">FEEDBACK</h1>
-                <p>Valorizamos sua opinião! Seu feedback é fundamental para nós. Por favor, dedique alguns momentos para compartilhar suas impressões e sugestões. Sua contribuição nos ajuda a melhorar nossos serviços e oferecer uma experiência ainda mais satisfatória. Agradecemos desde já por sua participação</p></section>
+                <p>Valorizamos sua opinião! Seu feedback é fundamental para nós. Por favor, dedique alguns momentos para compartilhar suas impressões e sugestões. Sua contribuição nos ajuda a melhorar nossos serviços e oferecer uma experiência ainda mais satisfatória. Agradecemos desde já por sua participação</p>
+                <button className="btn">INICIAR</button>
+                </section>
             <section><img src={ImageHome} alt="" /></section>
         </div>
     )
