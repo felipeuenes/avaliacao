37932a35c28d9fd49efc63a2e6400ed0629395ee
Felipe Uenes <uenesdesigner@gmail.com>
2024-03-26 20:16:41 -0300

    add

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,7 +8,10 @@ export function Home(){
     
     return(
         <div className="displayHome">
-            <section><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis reprehenderit adipisci animi tempore explicabo accusantium eos fugiat deserunt, ducimus necessitatibus voluptate est aspernatur! Dolores ipsa odio, deleniti fugit quaerat aliquid.</p></section>
+            <section>
+                <h2 className="titulo1">Deixe aqui o seu</h2>
+                <h1 className="titulo2">FEEDBACK</h1>
+                <p>Valorizamos sua opinião! Seu feedback é fundamental para nós. Por favor, dedique alguns momentos para compartilhar suas impressões e sugestões. Sua contribuição nos ajuda a melhorar nossos serviços e oferecer uma experiência ainda mais satisfatória. Agradecemos desde já por sua participação</p></section>
             <section><img src={ImageHome} alt="" /></section>
         </div>
     )
